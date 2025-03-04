f43a17f888222ed05c459bb7c1d2f6df849f4e93
anaseller <seller.anst@gmail.com>
2025-03-04 11:24:57 +0300

    New commit

--- python/hw10_task2.py
+++ python/hw10_task2.py
@@ -8,3 +8,22 @@
 # Все символы в строке уникальны.
 # Введите строку: Hello
 # Символы 'l' и 'o' повторяются.
+
+text = input('Введите строку: ')
+repeated_chars = ''
+unique = True
+
+i = 0
+while i < len(text):
+    j = i + 1
+    while j < len(text):
+        if text[i] == text[j] and text[i] not in repeated_chars:
+            repeated_chars += text[i] + ' '
+            unique = False
+        j += 1
+    i += 1
+
+if unique:
+    print('Все символы в строке уникальны.')
+else:
+    print('Символы "' + repeated_chars.strip().replace(' ', '", "') + '" повторяются.')
