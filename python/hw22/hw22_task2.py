1f6dda46341305b7c967407ac8c71f7162aed16e
anaseller <seller.anst@gmail.com>
2025-05-13 23:47:17 +0200

    New commit

--- python/hw22/hw22_task2.py
+++ python/hw22/hw22_task2.py
@@ -3,3 +3,32 @@
 (ValueError, ZeroDivisionError). Используйте конструкцию try-except-finally для обработки исключений
 и закрытия файла в блоке finally."""
 
+def process_file(filename):
+    file = None
+    try:
+        file = open(filename, 'r')
+        lines = file.readlines()
+
+        for line in lines:
+            parts = line.strip().split()
+
+            try:
+                num1 = float(parts[0])
+                num2 = float(parts[1])
+                result = num1 / num2
+                print(f'{num1} / {num2} = {result}')
+            except ZeroDivisionError:
+                print(f'Ошибка: Деление на ноль невозможно')
+            except ValueError:
+                print(f'Ошибка: Невозможно преобразовать строку в число')
+            except IndexError:
+                print(f'Ошибка: В строке недостаточно чисел')
+
+    except FileNotFoundError:
+        print(f'Ошибка: Файл "{filename}" не найден')
+
+    finally:
+        if file:
+            file.close()
+
+process_file('ex_data.txt')
